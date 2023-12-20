05819931a8b42d83107fce96953598fbb5241ef8
songmoro <wlqgkr@naver.com>
2023-12-21 00:38:48 +0900

    Initial Commit

---
+++ CoreImageGaussianFilter/CoreImageGaussianFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageGaussianFilterApp.swift
+//  CoreImageGaussianFilter
+//
+//  Created by 송재훈 on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageGaussianFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
